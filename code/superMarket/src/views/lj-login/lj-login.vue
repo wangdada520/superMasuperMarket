c7bb7c38f86386ee3986006a37c9d9cb10e56ee4
dadalijie <913410518@qq.com>
2019-06-13 11:37:24 +0800

    上传李杰的登录页面

---
+++ code/superMarket/src/views/lj-login/lj-login.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="login">
+        <div class="login-wrap">
+            <h1><span class="el-icon-menu"></span> 华联超市管理系统</h1>
+            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                <el-form-item label="账户" prop="account">
+                    <el-input type="text" v-model="ruleForm.account" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="pass">
+                    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" prop="checkPass">
+                    <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm()">登录</el-button>
+                    <el-button @click="resetForm()">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+<script>
+// 引入密码正则验证
+import { passwordReg } from '@/utils/validator.js'
+import { all } from 'q';
+
+export default {
+    data(){
+        // 确认密码自定义验证
+        var confirmPassword = (rule, value, callback) => {
+            if(value === ''){
+                callback(new Error('不能为空'))
+            }else if(value !== this.ruleForm.pass){
+                callback(new Error('两次密码不一致'));
+            }else{
+                callback();
+            }
+        }
+        // 密码自定义验证
+        var checkPassword = (rule, value, callback) => {
+            if(value === ''){
+                callback(new Error('不能为空'))
+            }else if(value.length<3 || value.length>6){
+                callback(new Error('密码在 3~6 位之间'))
+            }else if(!passwordReg(value)){
+                callback(new Error('密码必须是数字和字母组合'))
+            }else{
+                if(this.ruleForm.checkPass !== ''){
+                    this.$refs.ruleForm.validateField('checkPass');
+                }
+                callback();
+            }
+        }
+        return {
+            // 表单数据
+            ruleForm:{
+                account:'',
+                pass: '',
+                checkPass: ''
+            },
+            // 验证规则
+            rules:{
+                // 账户
+                account: [
+                    {required:true,message:"请输入账户",trigger:"blur"},
+                    {min:3,max:6,message:"账户长度3-6",trigger:"blur"}
+                ],
+                // 密码
+                pass: [
+                    {required:true,validator:checkPassword,trigger:"blur"}
+                ],
+                // 确认密码
+                checkPass: [
+                    {required:true,validator:confirmPassword,trigger:"blur"}
+                ]
+            }
+        }
+    },
+    methods:{
+        // 登录
+        submitForm() {
+            this.$refs.ruleForm.validate((valid) => {
+                if (valid) {
+                    let params ={
+                        account:this.ruleForm.account,
+                        password:this.ruleForm.pass
+                    }
+                    alert('登陆成功');
+                    this.$router.push('/home');
+                } else {
+                    console.log('前段验证不通过，不允许提交');
+                    return false;
+                }
+            });
+        },
+        //   重置
+        resetForm() {
+            this.$refs.ruleForm.resetFields();
+        }
+    }
+}
+</script>
+<style lang="less">
+     @import './login.less';
+</style>
+
+
